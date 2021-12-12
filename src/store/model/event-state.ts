2c59a79bb55e92c6d102714b5b4d1b377d6f799c
barimale <mateusz.wolnica@gmail.com>
2021-12-12 19:17:10 +0100

    .

--- src/store/model/event-state.ts
+++ src/store/model/event-state.ts
@@ -1,7 +1,9 @@
+/* eslint-disable no-unused-vars */
+
 /**
- * This is a TypeGen auto-generated file.
- * Any changes made to this file can be lost when this file is regenerated.
- */
+  * This is a TypeGen auto-generated file.
+  * Any changes made to this file can be lost when this file is regenerated.
+  *  */
 
  enum EventState {
   Created = 0,
--- src/store/model/event.ts
+++ src/store/model/event.ts
@@ -1,9 +1,11 @@
-/**
- * This is a TypeGen auto-generated file.
- * Any changes made to this file can be lost when this file is regenerated.
- */
+/* eslint-disable no-unused-vars */
 
-import EventState from "./event-state";
+/**
+  * This is a TypeGen auto-generated file.
+  * Any changes made to this file can be lost when this file is regenerated.
+  *  */
+
+import EventState from './event-state';
 
 interface Event {
   id: string | null | undefined;
