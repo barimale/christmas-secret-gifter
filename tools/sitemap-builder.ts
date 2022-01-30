f7d5f0f2c8cc37d9d55bfebc43a0fdb609e22a12
barimale <mateusz.wolnica@gmail.com>
2022-01-30 12:10:10 +0100

    .

--- tools/sitemap-builder.ts
+++ tools/sitemap-builder.ts
@@ -4,7 +4,7 @@ import Routes from '../src/router/Routes';
 interface RouterRoute { props: { path: string } }
 interface SitemapRoute { path: string }
 
-const PUBLIC_URL = 'https://odkrywajcie.pl';
+const PUBLIC_URL = 'https://christmas-secret-gifter.web.app';
 
 const routes = (Routes()?.props.children || [])
   .reduce((acc: SitemapRoute[], route: RouterRoute) => {
