d4b29186bc748ea12ec84969f8ba428ca50dc05c
barimale <mateusz.wolnica@gmail.com>
2022-02-12 21:35:06 +0100

    .

--- src/components/organisms/MatchParticipants.tsx
+++ src/components/organisms/MatchParticipants.tsx
@@ -6,11 +6,19 @@ import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableContainer from '@mui/material/TableContainer';
 import TableRow from '@mui/material/TableRow';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import Divider from '@mui/material/Divider';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { DeviceContextConsumer, DeviceType, EventContext } from '../../contexts';
 import AlgorithmResponse from '../../store/model/algorithm-response';
 import CenteredDiv from '../templates/CenteredDiv';
 import { CopyPairsToClipboard } from '../molecules/CopyPairsToClipboard';
+import Pair from '../../store/model/pair';
 
 const MatchParticipants = () => {
   const { participants, analyze } = useContext(EventContext);
@@ -137,26 +145,15 @@ const MatchParticipants = () => {
                                   `${getName(r.fromIndex)} is going to buy a gift to ${getName(r.toIndex)}\n`))
                                 }
                               />
-                              <ol style={{
+                              <List style={{
                                 fontSize: context.valueOf() === DeviceType.isDesktopOrLaptop ? '16px' : '12px',
                                 marginTop: '0px',
                               }}
                               >
                                 {response.pairs.flatMap((r) => (
-                                  <li style={{
-                                    fontSize: context.valueOf() === DeviceType.isDesktopOrLaptop ? '16px' : '12px',
-                                  }}
-                                  >
-                                    <span
-                                      style={{
-                                        fontSize: context.valueOf() === DeviceType.isDesktopOrLaptop ? '16px' : '12px',
-                                      }}
-                                    >
-                                      {`${getName(r.fromIndex)} is going to buy a gift to ${getName(r.toIndex)}`}
-                                    </span>
-                                  </li>
+                                  <PairRow pair={r} />
                                 ))}
-                              </ol>
+                              </List>
                             </>
                           )}
                         </TableCell>
@@ -172,6 +169,113 @@ const MatchParticipants = () => {
             )}
           </Paper>
         </div>
+      )}
+    </DeviceContextConsumer>
+  );
+};
+
+interface PairRowProps{
+  pair: Pair;
+}
+
+const PairRow = (props: PairRowProps) => {
+  const { pair } = props;
+  const { participants } = useContext(EventContext);
+
+  function getName (orderId: number): string | undefined | null {
+    const found = participants[orderId];
+
+    if (found !== undefined) {
+      return found.name;
+    }
+    return undefined;
+  }
+
+  function isLast (orderId: number): boolean {
+    const found = participants[orderId];
+    const last = participants[participants.length];
+
+    if (found !== undefined && last !== undefined) {
+      return found.id === last.id;
+    }
+
+    return false;
+  }
+
+  function stringToColor (input: string) {
+    let hash = 0;
+    let i;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < input.length; i += 1) {
+      hash = input.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = '#';
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.substr(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
+  }
+
+  function stringAvatar (name: string) {
+    const splitted = name.split(' ');
+    const firstLettersOnly = splitted.flatMap((p) => p[0]).join('');
+
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: `${firstLettersOnly.slice(0, 2)}`,
+    };
+  }
+
+  const gifterName = getName(pair.fromIndex) ?? '';
+
+  return (
+    <DeviceContextConsumer>
+      {(context) => (
+        <>
+          <ListItem
+            alignItems="flex-start"
+            style={{
+              fontSize: context.valueOf() === DeviceType.isDesktopOrLaptop ? '16px' : '12px',
+            }}
+          >
+            <ListItemAvatar>
+              <Avatar alt={gifterName} {...stringAvatar(gifterName)} />
+            </ListItemAvatar>
+            <ListItemText
+              primaryTypographyProps={{
+                fontWeight: 'bold',
+                fontSize: context.valueOf() === DeviceType.isDesktopOrLaptop ? '20px' : '14px',
+              }}
+              primary={gifterName}
+              secondary={(
+                <Typography
+                  sx={{
+                    display: 'inline',
+                  }}
+                  style={{
+                    fontSize: context.valueOf() === DeviceType.isDesktopOrLaptop ? '16px' : '12px',
+                  }}
+                  component="span"
+                  variant="body2"
+                  color="text.primary"
+                >
+                  {`is going to buy a gift to ${getName(pair.toIndex)}`}
+                </Typography>
+          )}
+            />
+          </ListItem>
+          {!isLast(pair.fromIndex) && (
+          <Divider variant="inset" component="li" />
+          )}
+        </>
       )}
     </DeviceContextConsumer>
   );
