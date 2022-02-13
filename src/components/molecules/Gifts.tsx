6f4d3fc1ffb61d00110dd282876d488256962b95
barimale <mateusz.wolnica@gmail.com>
2022-02-13 23:11:52 +0100

    .

--- src/components/molecules/Gifts.tsx
+++ src/components/molecules/Gifts.tsx
@@ -1,70 +1,75 @@
 import * as React from 'react';
+import { DeviceContextConsumer, DeviceType } from '../../contexts';
 
 interface GiftsProps {
   style: React.CSSProperties;
 }
 
 export const Gifts = (props: GiftsProps) => (
-  <div style={props.style}>
-    <div style={{
-      width: 'auto',
-      height: 'auto',
-      fontFamily: 'kgChristmasTrees',
-      color: 'whitesmoke',
-      gap: '10px',
-      display: 'flex',
-      flexDirection: 'row',
-      justifyItems: 'stretch',
-      alignItems: 'center',
-      fontSize: '30px',
-    }}
-    >
-      <p style={{
-        color: 'whitesmoke',
-        fontFamily: 'inherit',
-        margin: '0px',
-        padding: '0px',
-        height: 'fit-content',
-      }}
-      >
-        D
-      </p>
-      <p style={{
-        color: 'whitesmoke',
-        fontFamily: 'inherit',
-        margin: '0px',
-        padding: '0px',
-        height: 'fit-content',
-      }}
-      >
-        D
-      </p>
-      <p style={{
-        color: 'whitesmoke',
-        fontFamily: 'inherit',
-        margin: '0px',
-        padding: '0px',
-        height: 'fit-content',
-      }}
-      >
-        D
-      </p>
-      <p style={{
-        color: 'whitesmoke',
-        fontFamily: 'inherit',
-        margin: '0px',
-        padding: '0px',
-        height: 'fit-content',
-      }}
-      >
-        D
-      </p>
-      <p style={{
-        fontFamily: 'inherit', margin: '0px', padding: '0px', height: 'fit-content', color: 'whitesmoke',
-      }}
-      >
-        D
-      </p>
-    </div>
-  </div>
+  <DeviceContextConsumer>
+    {(context) => (
+      <div style={props.style}>
+        <div style={{
+          width: 'auto',
+          height: 'auto',
+          fontFamily: 'kgChristmasTrees',
+          color: 'whitesmoke',
+          gap: '10px',
+          display: 'flex',
+          flexDirection: 'row',
+          justifyItems: 'stretch',
+          alignItems: 'center',
+          fontSize: context.valueOf() === DeviceType.isDesktopOrLaptop ? '30px' : '16px',
+        }}
+        >
+          <p style={{
+            color: 'whitesmoke',
+            fontFamily: 'inherit',
+            margin: '0px',
+            padding: '0px',
+            height: 'fit-content',
+          }}
+          >
+            D
+          </p>
+          <p style={{
+            color: 'whitesmoke',
+            fontFamily: 'inherit',
+            margin: '0px',
+            padding: '0px',
+            height: 'fit-content',
+          }}
+          >
+            D
+          </p>
+          <p style={{
+            color: 'whitesmoke',
+            fontFamily: 'inherit',
+            margin: '0px',
+            padding: '0px',
+            height: 'fit-content',
+          }}
+          >
+            D
+          </p>
+          <p style={{
+            color: 'whitesmoke',
+            fontFamily: 'inherit',
+            margin: '0px',
+            padding: '0px',
+            height: 'fit-content',
+          }}
+          >
+            D
+          </p>
+          <p style={{
+            fontFamily: 'inherit', margin: '0px', padding: '0px', height: 'fit-content', color: 'whitesmoke',
+          }}
+          >
+            D
+          </p>
+        </div>
+      </div>
+    )}
+  </DeviceContextConsumer>
 );
