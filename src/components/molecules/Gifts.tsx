b802527a468c48fb01d96a9ee6fa26324d7f13f6
barimale <mateusz.wolnica@gmail.com>
2022-02-13 23:15:01 +0100

    .

--- src/components/molecules/Gifts.tsx
+++ src/components/molecules/Gifts.tsx
@@ -14,7 +14,7 @@ export const Gifts = (props: GiftsProps) => (
           height: 'auto',
           fontFamily: 'kgChristmasTrees',
           color: 'whitesmoke',
-          gap: '10px',
+          gap: context.valueOf() === DeviceType.isDesktopOrLaptop ? '10px' : '4px',
           display: 'flex',
           flexDirection: 'row',
           justifyItems: 'stretch',
