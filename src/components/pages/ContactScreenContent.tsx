bcee9de29c6bdd37fb2970c03652630da01f40e4
Mateusz Wolnica <mateusz.wolnica@gmail.com>
2023-10-27 09:49:43 +0000

    Updated ContactScreenContent.tsx

--- src/components/pages/ContactScreenContent.tsx
+++ src/components/pages/ContactScreenContent.tsx
@@ -1,12 +1,10 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import LaunchIcon from '@material-ui/icons/Launch';
-import { useTranslation } from 'react-i18next';
 import { DeviceContextConsumer, DeviceType } from '../../contexts/DeviceContext';
 import { Theme as customTheme } from '../../theme/custom-theme';
 
 export default function ContactScreenContent () {
-  const { t } = useTranslation();
 
   return (
     <DeviceContextConsumer>
@@ -55,46 +53,6 @@ export default function ContactScreenContent () {
               rel="noreferrer"
             >
               {'https://github.com/barimale'.toUpperCase()}
-              <LaunchIcon
-                style={{
-                  paddingLeft: context === DeviceType.isDesktopOrLaptop ? '6px' : '2px',
-                  height: context === DeviceType.isDesktopOrLaptop ? '32px' : '26px',
-                  width: 'auto',
-                  alignSelf: 'center',
-                  color: 'black',
-                  opacity: 'inherit',
-                }}
-              />
-            </a>
-            <a
-              className="pointerOverEffect"
-              href="https://en.wikipedia.org/wiki/Christmas_tree"
-              target="_blank"
-              rel="noreferrer"
-              style={{
-                color: 'white',
-                textDecoration: 'none',
-                cursor: 'pointer',
-                fontFamily: 'inherit',
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-                fontSize: context === DeviceType.isDesktopOrLaptop ? '20px' : '10px',
-                padding: context === DeviceType.isDesktopOrLaptop ? '20px' : '10px',
-                backgroundColor: `${customTheme.palette.primary.light}`,
-                borderRadius: context === DeviceType.isDesktopOrLaptop ? '20px' : '10px',
-              }}
-            >
-              <p style={{
-                padding: '0px',
-                margin: '0px',
-              }}
-              >
-                {t('Details.Line1')}
-                <i>{t('Details.Line2')}</i>
-                {t('Details.Line3')}
-              </p>
               <LaunchIcon
                 style={{
                   paddingLeft: context === DeviceType.isDesktopOrLaptop ? '6px' : '2px',
